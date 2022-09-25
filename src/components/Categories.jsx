039d7bf3ec7de4d75ed38678ea4e896d440c4bd0
Ilya Kulesh <ilyakulesh1@yandex.by>
2022-09-25 22:00:42 +0300

    added components

---
+++ src/components/Categories.jsx
@@ -0,0 +1,14 @@
+export default function Catigories() {
+    return (
+      <div class="categories">
+        <ul>
+          <li class="active">Все</li>
+          <li>Мясные</li>
+          <li>Вегетарианская</li>
+          <li>Гриль</li>
+          <li>Острые</li>
+          <li>Закрытые</li>
+        </ul>
+      </div>
+    );
+  }
