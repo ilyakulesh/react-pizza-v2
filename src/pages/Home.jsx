a136016c53c4fbc600b77a1077f3c9ffb7ecb403
Ilya Kulesh <ilyakulesh1@yandex.by>
2022-09-29 13:54:53 +0300

    created a shopping cart page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,32 +6,33 @@ import PizzaBlock from "../components/PizzaBlock";
 import Skeleton from "../components/PizzaBlock/Skeleton";
 
 const Home = () => {
-    const [items, setItems] = React.useState([]);
-    const [isLoading, setIsLoading] = React.useState(true);
-  
-    React.useEffect(() => {
-      fetch("https://63334d6b573c03ab0b5bcff0.mockapi.io/items")
-        .then((res) => res.json())
-        .then((json) => {
-          setItems(json);
-          setIsLoading(false);
-        });
-    }, []);
+  const [items, setItems] = React.useState([]);
+  const [isLoading, setIsLoading] = React.useState(true);
 
-    return (
-        <>
-        <div className="content__top">
-            <Catigories />
-            <Sort />
-        </div>
-        <h2 className="content__title">Все пиццы</h2>
-        <div className="content__items">
-            {isLoading
-              ? [...new Array(6)].map((_, index) => <Skeleton key={index} />)
-              : items.map((obj) => <PizzaBlock key={obj.id} {...obj} />)}
-        </div>
-        </>
-    )
-}
+  React.useEffect(() => {
+    fetch("https://63334d6b573c03ab0b5bcff0.mockapi.io/items")
+      .then((res) => res.json())
+      .then((json) => {
+        setItems(json);
+        setIsLoading(false);
+      });
+    window.scrollTo(0, 0);
+  }, []);
+
+  return (
+    <div className="container">
+      <div className="content__top">
+        <Catigories />
+        <Sort />
+      </div>
+      <h2 className="content__title">Все пиццы</h2>
+      <div className="content__items">
+        {isLoading
+          ? [...new Array(6)].map((_, index) => <Skeleton key={index} />)
+          : items.map((obj) => <PizzaBlock key={obj.id} {...obj} />)}
+      </div>
+    </div>
+  );
+};
 
 export default Home;
