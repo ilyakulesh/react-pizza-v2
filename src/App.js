e19c9c2d797673504d66273ff94437d2a4f293f6
Ilya Kulesh <ilyakulesh1@yandex.by>
2022-10-04 22:25:13 +0300

    added a useRef hook

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { useSelector, useDispatch } from "react-redux";
 import { Routes, Route } from "react-router-dom";
 import "./scss/app.scss";
 
--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -5,6 +5,12 @@ import { SearchContext } from "../../App";
 
 const Search = () => {
   const { searchValue, setSearchValue } = React.useContext(SearchContext);
+  const inputRef = React.useRef();
+
+  const onClickClear = () => {
+    setSearchValue("");
+    inputRef.current.focus();
+  };
 
   return (
     <div className={styles.root}>
@@ -43,6 +49,7 @@ const Search = () => {
         />
       </svg>
       <input
+        ref={inputRef}
         value={searchValue}
         onInput={(event) => setSearchValue(event.target.value)}
         className={styles.input}
@@ -50,7 +57,7 @@ const Search = () => {
       />
       {searchValue && (
         <svg
-          onClick={() => setSearchValue("")}
+          onClick={onClickClear}
           className={styles.clearIcon}
           viewBox="0 0 20 20"
           xmlns="http://www.w3.org/2000/svg"
