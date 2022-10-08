31fa4733183e2a64c69bfc10365d5b4d7899c76b
Ilya Kulesh <ilyakulesh1@yandex.by>
2022-10-08 20:45:26 +0300

    bug fixed: now the number of pizzas in the cart is displayed right

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,6 +7,8 @@ import Search from "./Search";
 export default function Header() {
   const { items, totalPrice } = useSelector((state) => state.cart);
 
+  const totalCount = items.reduce((sum, item) => sum + item.count, 0);
+
   return (
     <div className="header">
       <div className="container">
@@ -53,7 +55,7 @@ export default function Header() {
                 strokeLinejoin="round"
               />
             </svg>
-            <span>{items.length}</span>
+            <span>{totalCount}</span>
           </Link>
         </div>
       </div>
