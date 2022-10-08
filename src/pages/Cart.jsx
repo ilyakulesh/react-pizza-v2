35f4b1b3c2b1922a72d314a5240f970c5ef35d49
Ilya Kulesh <ilyakulesh1@yandex.by>
2022-10-08 22:19:25 +0300

    added CartEmpty component

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import CartItem from "../components/CartItem";
 import { clearItem } from "../redux/slices/cartSlice";
+import CartEmpty from "../components/CartEmpty";
 
 const Cart = () => {
   const dispatch = useDispatch();
@@ -15,6 +16,10 @@ const Cart = () => {
       dispatch(clearItem());
     }
   };
+
+  if (!totalPrice) {
+    return <CartEmpty />;
+  }
 
   return (
     <div className="container container--cart">
